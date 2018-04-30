2f42405af3171d1a15fd055be65ffe109f7ad64b
MinJaeSim <mj9757@gmail.com>
2018-04-30 10:13:12 +0000

    DB 정보 EC2 에 맞게 수정

--- api/person.js
+++ api/person.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : '',
+    password : 'mysql',
     database : 'db_project'
 });
 connection.connect();
