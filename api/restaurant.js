760d112d9aeed946a8eb2047a1633e4b62f29942
MinJaeSim <mj9757@gmail.com>
2018-06-01 16:28:23 +0000

    complex query 작성

--- api/restaurant.js
+++ api/restaurant.js
@@ -67,8 +67,13 @@ router.get('/simple', async (req, res, next) => {
 
 router.get('/complex', async (req, res, next) => {
     try {
-        const restaurant = await getDataFromDB(`SELECT DISTINCT Url.RNumber, KName, Grade, Phone_Num, Homepage, Price, Image1, Image2, Image3, Cat_Kor as Category FROM Michelin_Kor, Michelin_Location, Url, Dish_Category Where Michelin_Kor.RNumber = Michelin_Location.RNumber AND Url.RNumber = Michelin_Kor.RNumber AND Url.RNumber = Dish_Category.RNumber AND Cat_Kor LIKE "%${req.query['cat']}%" AND Location_Kor LIKE "%${req.query['loc']}%" AND Price >= ${req.query['min']} AND Price <= ${req.query['max']} AND Grade = ${req.query['grade']} GROUP BY Url.RNumber`);
-        res.status(200).json(restaurant);     
+	console.log("loc " + req.query['loc']);
+	console.log("cat " + req.query['cat']);
+	console.log("min " + req.query['min']);
+	console.log("max " + req.query['max']);
+	console.log("grade " + req.query['grade']);
+	const restaurant = await getDataFromDB(`SELECT DISTINCT Url.RNumber, KName, Grade, Phone_Num, Homepage, Price, Image1, Image2, Image3, Cat_Kor as Category FROM Michelin_Kor, Michelin_Location, Url, Dish_Category Where Michelin_Kor.RNumber = Michelin_Location.RNumber AND Url.RNumber = Michelin_Kor.RNumber AND Url.RNumber = Dish_Category.RNumber AND Cat_Kor LIKE "%${req.query['cat']}%" AND Location_Kor LIKE "%${req.query['loc']}%" AND Price >= ${req.query['min'] * 1} AND Price <= ${req.query['max'] * 1} AND Grade = ${req.query['grade'] * 1} GROUP BY Url.RNumber`);
+	res.status(200).json(restaurant);     
     }  catch(err) {
         res.status(400).json({message: err.message});
     }
