3d18c75e6ee6dd2bab33066078d5bd485ea71e0f
MinJaeSim <mj9757@gmail.com>
2018-06-04 17:27:13 +0900

    에러 수정

--- api/restaurant.js
+++ api/restaurant.js
@@ -65,7 +65,7 @@ router.get('/ko/complex', async (req, res, next) => {
 	console.log("min " + req.query['min']);
 	console.log("max " + req.query['max']);
     console.log("grade " + req.query['grade']);
-    const restaurant;
+    let restaurant;
     if(req.query['grade'] == -1)
         restaurant = await getDataFromDB(`SELECT DISTINCT Url.RNumber, KName as Name, Grade, Phone_Num, Homepage, Price, Image1, Image2, Image3, Cat_Kor as Category FROM Michelin_Kor, Michelin_Location, Url, Dish_Category Where Michelin_Kor.RNumber = Michelin_Location.RNumber AND Url.RNumber = Michelin_Kor.RNumber AND Url.RNumber = Dish_Category.RNumber AND Cat_Kor LIKE "%${req.query['cat']}%" AND Location_Kor LIKE "%${req.query['loc']}%" AND Price >= ${req.query['min']} AND Price <= ${req.query['max']} GROUP BY Url.RNumber`);
     else
@@ -114,7 +114,7 @@ router.get('/en/complex', async (req, res, next) => {
 	console.log("min " + req.query['min']);
 	console.log("max " + req.query['max']);
     console.log("grade " + req.query['grade']);
-    const restaurant;
+    let restaurant;
     if(req.query['grade'] == -1)
         restaurant = await getDataFromDB(`SELECT DISTINCT Url.RNumber, EName as Name, Grade, Phone_Num, Homepage, Price, Image1, Image2, Image3, Cat_Eng as Category FROM Michelin_Eng, Michelin_Location, Url, Dish_Category Where Michelin_Eng.RNumber = Michelin_Location.RNumber AND Url.RNumber = Michelin_Eng.RNumber AND Url.RNumber = Dish_Category.RNumber AND Cat_Eng LIKE "%${req.query['cat']}%" AND Location_Eng LIKE "%${req.query['loc']}%" AND Price >= ${req.query['min']} AND Price <= ${req.query['max']} GROUP BY Url.RNumber`);
     else 
