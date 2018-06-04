1f88f72eeca0bb0b24c635da687225b17eb711f2
MinJaeSim <mj9757@gmail.com>
2018-06-04 15:33:42 +0900

    영문 버전 api추가

--- api/restaurant.js
+++ api/restaurant.js
@@ -25,7 +25,7 @@ async function getDataFromDB(query){
     connection.end();
 }
 
-router.get('/', async (req, res, next) => {
+router.get('/ko/', async (req, res, next) => {
     try {
         const restaurant = await getDataFromDB("SELECT * FROM Michelin_Kor");
         const url = await getDataFromDB("SELECT * FROM Url");
@@ -43,7 +43,7 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-router.get('/simple', async (req, res, next) => {
+router.get('/ko/simple', async (req, res, next) => {
     try {
         if(req.query['loc'].length > 0) {
             console.log("loc " + req.query['loc']);
@@ -65,7 +65,7 @@ router.get('/simple', async (req, res, next) => {
     }
 });
 
-router.get('/complex', async (req, res, next) => {
+router.get('/ko/complex', async (req, res, next) => {
     try {
 	console.log("loc " + req.query['loc']);
 	console.log("cat " + req.query['cat']);
@@ -79,41 +79,56 @@ router.get('/complex', async (req, res, next) => {
     }
 });
 
-router.get('/test', async (req, res, next) => {
-    // 이름으로 검색
-    // 가격, 등급, 지역, 요리종류
-    //  /a/restaurant/test?a=1&b=2
-    console.log(req.query);
-    const keys = Object.keys(req.query)
-    console.log(keys);
-    console.log(req.query['c']);
-    if(req.query['c'] == null){
-        console.log("TEST");
-    }
-
-    res.status(200).json({"m" : "test"});
-    return;
+router.get('/en/', async (req, res, next) => {
     try {
-        connection.query("SELECT * FROM Michelin_Kor", function(err, rows, fields) {
-            if (!err) {
-                console.log('The solution is: ', rows);
-		        res.status(200).json(rows);
-	        }else
-                console.log('Error while performing Query.', err);
-        });
-    } catch (err) {
-        message: err.message;
-    }
+        const restaurant = await getDataFromDB("SELECT * FROM Michelin_Eng");
+        const url = await getDataFromDB("SELECT * FROM Url");
+        const category = await getDataFromDB("SELECT * FROM Dish_Category");
 
-    connection.end();
+        for(let i = 0; i < restaurant.length; i++) {
+            restaurant[i]['Image1'] = url[i]['Image1'];
+            restaurant[i]['Image2'] = url[i]['Image2'];
+            restaurant[i]['Image3'] = url[i]['Image3'];
+            restaurant[i]['Category'] = category[i]['Cat_Eng'];
+        }
+        res.status(200).json(restaurant);
+    } catch(err) {
+        res.status(400).json({message: err.message});
+    }
 });
 
-router.get('/test2', async (req, res, next) => {
+router.get('/en/simple', async (req, res, next) => {
     try {
-        const result1 = await getDataFromDB("SELECT * FROM DEPARTMENT");
-        const result2 = await getDataFromDB("SELECT * FROM PROJECT");
-        res.status(200).json(result1);
-    } catch(err) {
+        if(req.query['loc'].length > 0) {
+            console.log("loc " + req.query['loc']);
+            const restaurant = await getDataFromDB(`SELECT DISTINCT Url.RNumber, EName, Grade, Phone_Num, Homepage, Price, Image1, Image2, Image3, Cat_Eng as Category FROM Michelin_Eng, Michelin_Location, Url, Dish_Category Where Michelin_Eng.RNumber = Michelin_Location.RNumber AND Url.RNumber = Michelin_Eng.RNumber AND Url.RNumber = Dish_Category.RNumber AND Location_Eng LIKE "%${req.query['loc']}%" GROUP BY Url.RNumber`);
+            res.status(200).json(restaurant);
+        }
+        if (req.query['cat'].length > 0) {
+            console.log("cat " + req.query['cat']);
+            const restaurant = await getDataFromDB(`SELECT DISTINCT Url.RNumber, EName, Grade, Phone_Num, Homepage, Price, Image1, Image2, Image3, Cat_Eng as Category FROM Michelin_Eng, Michelin_Location, Url, Dish_Category Where Michelin_Eng.RNumber = Michelin_Location.RNumber AND Url.RNumber = Michelin_Eng.RNumber AND Url.RNumber = Dish_Category.RNumber AND Cat_Eng LIKE "%${req.query['cat']}%" GROUP BY Url.RNumber`);
+            res.status(200).json(restaurant);
+        }
+        if (req.query['name'].length > 0) {
+            console.log("name " + req.query['name']);
+            const restaurant = await getDataFromDB(`SELECT DISTINCT Url.RNumber, EName, Grade, Phone_Num, Homepage, Price, Image1, Image2, Image3, Cat_Eng as Category FROM Michelin_Eng, Michelin_Location, Url, Dish_Category Where Michelin_Eng.RNumber = Michelin_Location.RNumber AND Url.RNumber = Michelin_Eng.RNumber AND Url.RNumber = Dish_Category.RNumber AND EName LIKE "%${req.query['name']}%" GROUP BY Url.RNumber`);
+            res.status(200).json(restaurant);
+        }
+    }  catch(err) {
+        res.status(400).json({message: err.message});
+    }
+});
+
+router.get('/en/complex', async (req, res, next) => {
+    try {
+	console.log("loc " + req.query['loc']);
+	console.log("cat " + req.query['cat']);
+	console.log("min " + req.query['min']);
+	console.log("max " + req.query['max']);
+	console.log("grade " + req.query['grade']);
+	const restaurant = await getDataFromDB(`SELECT DISTINCT Url.RNumber, EName, Grade, Phone_Num, Homepage, Price, Image1, Image2, Image3, Cat_Eng as Category FROM Michelin_Eng, Michelin_Location, Url, Dish_Category Where Michelin_Eng.RNumber = Michelin_Location.RNumber AND Url.RNumber = Michelin_Eng.RNumber AND Url.RNumber = Dish_Category.RNumber AND Cat_Eng LIKE "%${req.query['cat']}%" AND Location_Eng LIKE "%${req.query['loc']}%" AND Price >= ${req.query['min'] * 1} AND Price <= ${req.query['max'] * 1} AND Grade = ${req.query['grade'] * 1} GROUP BY Url.RNumber`);
+	res.status(200).json(restaurant);     
+    }  catch(err) {
         res.status(400).json({message: err.message});
     }
 });
